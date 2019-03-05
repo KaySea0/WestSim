6a6d776214805896dec87efaeb03834d00319653
Kyle Cook <kcook2499@flpoly.org>
2019-03-05 08:47:56 -0500

    email send / signature confirmation

--- test.py
+++ test.py
@@ -2,39 +2,69 @@
 # https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f - how to send emails automatically
 import tkinter as tk
 import openpyxl
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
-# MY_ADDRESS = "info@westsiminc.com"
-# MY_PASSWORD = "Sukkur%%1798"
+MY_ADDRESS = "info@westsiminc.com"
+MY_PASSWORD = "Sukkur%%1798"
 
-# s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-# s.starttls()
-# s.login(MY_ADDRESS,MY_PASSWORD)
+# wb = openpyxl.load_workbook('CRHT_0228100406 (1).xlsx')
+# ws = wb.active
+# max_row = ws.max_row
 
-# msg = MIMEMultipart()
+s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+s.starttls()
+s.login(MY_ADDRESS,MY_PASSWORD)
 
-# message = "This is a test email. Lets see if we can send a response to this!"
+msg = MIMEMultipart('related')
 
-# msg['From'] = MY_ADDRESS
-# msg['To'] = "k.cook2499@gmail.com"
-# msg['Subject'] = "Test Email from Python"
+message = "This is a test email. Lets see if we can send a response to this!"
+signature = """Best, <br>\
+<p style="color: blue;">Kyle Cook <br>
+WestSim Engineering, Inc. <br>
+7061 Grand National Dr. Suite 107A <br>
+Orlando, FL 32819 <br></p>
+<p> info@westsiminc.com <br>
+Phone: (407) 963-6699 <br>
+Fax: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(407) 203-4264 <br>
+http://www.westsiminc.com/ <br>
+<img src="cid:logo"> <br>
+</p>
+"""
 
-# msg.attach(MIMEText(message,'plain'))
+msg['From'] = MY_ADDRESS
+msg['To'] = "k.cook2499@gmail.com"
+msg['Subject'] = "Test Email from Python"
 
-# s.send_message(msg)
-# del msg
+msgBody = MIMEMultipart('alternative')
+msg.attach(msgBody)
 
-# s.quit()
+msgBody.attach(MIMEText(message,'plain'))
+msgBody.attach(MIMEText(signature,'html'))
+
+fp = open('logo.png','rb')
+img = MIMEImage(fp.read())
+fp.close()
+img.add_header('Content-ID', '<logo>')
+msg.attach(img)
+
+s.send_message(msg)
+del msg
+
+s.quit()
 
 
 
 
 # how to open / edit / save workbooks
-wb = openpyxl.load_workbook('Ship-Invoice-02142019.xlsx')
-ws = wb["DLAORDERS"]
-print(ws.max_row)
+# wb = openpyxl.load_workbook('CRHT_0228100406 (1).xlsx')
+# ws = wb.active
+# max_row = ws.max_row
+# for i in range(1,max_row+1):
+	# print(ws.cell(None,i,23).value)
+
 # wb.save('Ship-Invoice-02142019.xlsx')
 
 
