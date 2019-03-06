1e4217d59dd673d249366a51fd159d7d8eb6d89d
Kyle Cook <kcook2499@flpoly.org>
2019-03-06 08:34:59 -0500

    initial tkinter window / encapsulation

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# https://www.lifewire.com/what-are-the-outlook-com-smtp-server-settings-1170671
+# https://stackoverflow.com/questions/15306631/how-do-i-create-child-windows-with-python-tkinter
 # https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f - how to send emails automatically
 import tkinter as tk
 import openpyxl
@@ -11,55 +11,44 @@ from string import Template
 MY_ADDRESS = "info@westsiminc.com"
 MY_PASSWORD = "Sukkur%%1798"
 
+def read_template(filename):
+	with open(filename, 'r',) as template_file:
+		template_content = template_file.read()
+	return Template(template_content)
+
 # wb = openpyxl.load_workbook('CRHT_0228100406 (1).xlsx')
 # ws = wb.active
 # max_row = ws.max_row
 
-s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-s.starttls()
-s.login(MY_ADDRESS,MY_PASSWORD)
+def send_emails():
+	s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+	s.starttls()
+	s.login(MY_ADDRESS,MY_PASSWORD)
 
-msg = MIMEMultipart('related')
+	msg = MIMEMultipart('related')
 
-signature = """<p> Please provide a quote for the following: <br> <br>\
-${PART_INFO} <br> 
-Best, <br> </p>
-<p style="color: blue;">Kyle Cook <br>
-WestSim Engineering, Inc. <br>
-7061 Grand National Dr. Suite 107A <br>
-Orlando, FL 32819 <br></p>
-<p> info@westsiminc.com <br>
-Phone: (407) 963-6699 <br>
-Fax: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(407) 203-4264 <br>
-http://www.westsiminc.com/ <br>
-<img src="cid:logo"> <br>
-</p>
-"""
+	msg['From'] = MY_ADDRESS
+	msg['To'] = "k.cook2499@gmail.com"
+	msg['Subject'] = "Test Quote"
 
-msg['From'] = MY_ADDRESS
-msg['To'] = "k.cook2499@gmail.com"
-msg['Subject'] = "Test Email from Python"
+	msgBody = MIMEMultipart()
+	msg.attach(msgBody)
 
-msgBody = MIMEMultipart('alternative')
-msg.attach(msgBody)
+	email_body = read_template('base_email.txt')
+	subMessage = email_body.substitute(PART_INFO = "P/N: 132456-789 <br> QTY: 2 <br>")
 
-subMessage = Template(signature).substitute(PART_INFO = "P/N: 132456-789 - QTY 2 <br>")
+	msgBody.attach(MIMEText(subMessage,'html'))
 
-msgBody.attach(MIMEText(subMessage,'html'))
+	fp = open('logo.png','rb')
+	img = MIMEImage(fp.read())
+	fp.close()
+	img.add_header('Content-ID', '<logo>')
+	msg.attach(img)
 
-fp = open('logo.png','rb')
-img = MIMEImage(fp.read())
-fp.close()
-img.add_header('Content-ID', '<logo>')
-msg.attach(img)
+	s.send_message(msg)
+	del msg
 
-s.send_message(msg)
-del msg
-
-s.quit()
-
-
-
+	s.quit()
 
 # how to open / edit / save workbooks
 # wb = openpyxl.load_workbook('CRHT_0228100406 (1).xlsx')
@@ -70,17 +59,38 @@ s.quit()
 
 # wb.save('Ship-Invoice-02142019.xlsx')
 
+def create_window():
+	t = tk.TopLevel(
+
 
 # generic tkinter initialization
-# root = tk.Tk()
+def init_window():
+	root = tk.Tk()
+	root.title("Westsim Engineering")
+	root.geometry('500x300')
+	
+	frame = tk.Frame(root)
+	frame.config(bg="white")
+	
+	rows = 0
+	while rows < 50:
+		frame.rowconfigure(rows,weight=1)
+		frame.columnconfigure(rows,weight=1)
+		rows += 1
+		
+	frame.pack(side=tk.LEFT,anchor="nw")
+	
+	canvas = tk.Canvas(frame,width=99,height=39)
+	canvas.grid(row=0,column=0)
+	img = tk.PhotoImage(file="logo.gif")
+	canvas.create_image(0,0, anchor="nw", image=img)
+	
+	test_button = tk.Button(frame,text="Create Test Spreadsheet!",fg="red",command = quit)
+	test_button.grid(row=1,column=0,padx=10,pady=10)
 
-# frame = tk.Frame(root)
-# frame.pack()
+	another_button = tk.Button(frame,text="Goodbye!",command=quit)
+	another_button.grid(row=1,column=1,padx=10,pady=10)
 
-# test_button = tk.Button(frame,text="Create Test Spreadsheet!",fg="red",command = create_sheet)
-# test_button.pack(side=tk.LEFT)
-
-# another_button = tk.Button(frame,text="Goodbye!",command=quit)
-# another_button.pack(side=tk.LEFT)
-
-# root.mainloop()
+	root.mainloop()
+	
+init_window()
