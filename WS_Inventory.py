cb52678bc8ffc35aa1e97249ebe7de6ea863dfc2
Kyle Cook <k.cook2499@gmail.com>
2019-04-25 16:35:07 -0400

    Inventory Edit functionaly finished

--- WS_Inventory.py
+++ WS_Inventory.py
@@ -26,16 +26,12 @@ class WS_Inventory(object):
 		self.canvas = None
 		self.scroll = None
 		
-	def reset_scrollregion(self, event):
-		self.canvas.configure(width=event.width, height=event.height, yscrollcommand=self.scroll.set)
-		
 	def update_view(self, *args):
 		
 		if self.search_frame is not None:
 			self.search_frame.destroy()
 			
 		self.search_frame = tk.Frame(self.canvas)
-		self.search_frame.bind("<Configure>", self.reset_scrollregion)
 		
 		pn_header = tk.Label(self.search_frame, text="PN")
 		pn_header.grid(row=0, column=0, padx=10, pady=10)
@@ -75,7 +71,7 @@ class WS_Inventory(object):
 			qty_search_label = tk.Label(self.search_frame, text=part[3])
 			qty_search_label.grid(row=i, column=2, padx=10, pady=10)
 			
-			edit_button = tk.Button(self.search_frame, text="Edit")
+			edit_button = tk.Button(self.search_frame, text="Edit", command=lambda item=part: self.edit_window(item))
 			edit_button.grid(row=i, column=3, padx=10, pady=10)
 			
 		self.canvas.create_window((4,4),window=self.search_frame,anchor="nw")
@@ -121,15 +117,53 @@ class WS_Inventory(object):
 		
 		messagebox.showinfo("Stock Added", "Stock information has been saved!")
 		
+	def edit_stock(self, index, win):
+		
+		self.inventory_list[index][1] = self.pn_var.get()
+		self.inventory_list[index][2] = self.nsn_var.get()
+		self.inventory_list[index][3] = self.qty_var.get()
+		
+		self.pn_var.set('')
+		self.nsn_var.set('')
+		self.qty_var.set(0)
+		
+		win.destroy()
+		messagebox.showinfo("Stock Edited", "Stock information has been saved!")
+		
 	def save_changes(self):
 		
 		with open("inventory.txt","wb") as fp:
 			pickle.dump(self.inventory_list, fp)
+			
+	def edit_window(self, item):
+		e_window = tk.Toplevel()
+		e_window.geometry('250x130')
+		e_window.title("Edit Stock")
 		
-	def show_inventory(self):
+		self.pn_var.set(item[1])
+		self.nsn_var.set(item[2])
+		self.qty_var.set(item[3])
 		
-		for part in self.inventory_list:
-			print(part[0],part[1],part[2],part[3])
+		pn_label = tk.Label(e_window, text="PN:")
+		pn_label.grid(row=0, column=0, padx=5, pady=5)
+		
+		pn_entry = tk.Entry(e_window, width=25, textvariable=self.pn_var)
+		pn_entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+		
+		nsn_label = tk.Label(e_window, text="NSN:")
+		nsn_label.grid(row=1, column=0, padx=5, pady=5)
+		
+		nsn_entry = tk.Entry(e_window, width=15, textvariable=self.nsn_var)
+		nsn_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
+		
+		qty_label = tk.Label(e_window, text="Quantity:")
+		qty_label.grid(row=2, column=0, padx=5, pady=5)
+		
+		qty_entry = tk.Entry(e_window, width=5, textvariable=self.qty_var)
+		qty_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+		
+		submit_button = tk.Button(e_window, text="Submit", command=lambda: self.edit_stock(item[0]-1, e_window))
+		submit_button.grid(row=3, column=1, padx=5, pady=5, sticky="w")
 	
 	def add_stock_window(self):
 		win = tk.Toplevel()
