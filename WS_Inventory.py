0a010566f0863657bf2267181a095bd085419314
Kyle <k.cook2499@gmail.com>
2019-05-08 08:55:51 -0400

    Inventory id feature / menu cleanup, Shipping planned out

--- WS_Inventory.py
+++ WS_Inventory.py
@@ -2,8 +2,7 @@ import tkinter as tk
 import pickle
 from tkinter import messagebox
 
-# distinguish between reference variables for 'add' and 'search/update' functionality
-# add id functionality 
+# change search function to work with ID once all stock has been marked
 
 class WS_Inventory(object):
 	
@@ -19,12 +18,12 @@ class WS_Inventory(object):
 		self.pn_var = tk.StringVar()  # reference variable for part number
 		self.nsn_var = tk.StringVar() # reference variable for NSN
 		self.qty_var = tk.IntVar()    # reference variable for quantity
-		self.id_var = tk.StringVar()
+		self.id_var = tk.StringVar()  # reference variable for stock id
 		
-		self.pn_add_var = tk.StringVar()
-		self.nsn_add_var = tk.StringVar()
-		self.qty_add_var = tk.IntVar()
-		self.id_add_var = tk.StringVar()
+		self.pn_add_var = tk.StringVar()  # reference variable for part number - addition
+		self.nsn_add_var = tk.StringVar() # reference variable for NSN - addition
+		self.qty_add_var = tk.IntVar()    # reference variable for quantity - addition
+		self.id_add_var = tk.StringVar()  # reference variable for stock id - addition
 		
 		self.search_frame = None # frame that contains list of stock that matches search
 		self.canvas = None # canvas that contains search frame
@@ -56,6 +55,10 @@ class WS_Inventory(object):
 		# label for QTY column
 		qty_header = tk.Label(self.search_frame, text="QTY")
 		qty_header.grid(row=0, column=2, padx=10, pady=10)
+		
+		# label for ID column
+		id_header = tk.Label(self.search_frame, text="ID")
+		id_header.grid(row=0, column=3, padx=10, pady=10)
 		
 		# if neither text box has any data, display full inventory list
 		if not self.pn_var.get() and not self.nsn_var.get():
@@ -100,9 +103,16 @@ class WS_Inventory(object):
 			qty_search_label = tk.Label(self.search_frame, text=part[3])
 			qty_search_label.grid(row=i, column=2, padx=10, pady=10)
 			
+			# ID label for stock
+			try:
+				id_search_label = tk.Label(self.search_frame, text=part[4])
+			except IndexError:
+				id_search_label = tk.Label(self.search_frame, text='')
+			id_search_label.grid(row=i, column=3, padx=10, pady=10)
+			
 			# 'Edit' button for each item to change 
 			edit_button = tk.Button(self.search_frame, text="Edit", command=lambda item=part: self.edit_window(item))
-			edit_button.grid(row=i, column=3, padx=10, pady=10)
+			edit_button.grid(row=i, column=4, padx=10, pady=10)
 			
 		self.canvas.create_window((4,4),window=self.search_frame,anchor="nw")
 		
@@ -114,7 +124,7 @@ class WS_Inventory(object):
 		
 	def search_window(self):
 		sw = tk.Toplevel()
-		sw.geometry('400x300')
+		sw.geometry('450x300')
 		sw.title("Search Inventory")
 		
 		self.canvas = tk.Canvas(sw, borderwidth=0)
@@ -134,16 +144,23 @@ class WS_Inventory(object):
 		nsn_entry = tk.Entry(input_frame, width=20, textvariable=self.nsn_var)
 		nsn_entry.grid(row=0, column=3, padx=5, pady=5)
 		
+		id_label = tk.Label(input_frame, text="ID:")
+		id_label.grid(row=0, column=4, padx=5, pady=5)
+		
+		id_entry = tk.Entry(input_frame, width=5, textvariable=self.id_var)
+		id_entry.grid(row=0, column=5, padx=5, pady=5)
+		
 		input_frame.pack(anchor="n", fill=tk.X)
 		self.update_view()
 		
 	def add_stock(self):
 		
-		self.inventory_list.append([len(self.inventory_list)+1, self.pn_var.get(), self.nsn_var.get(), self.qty_var.get()])
+		self.inventory_list.append([len(self.inventory_list)+1, self.pn_add_var.get(), self.nsn_add_var.get(), self.qty_add_var.get(), self.id_add_var.get()])
 		
-		self.pn_var.set('')
-		self.nsn_var.set('')
-		self.qty_var.set(0)
+		self.pn_add_var.set('')
+		self.nsn_add_var.set('')
+		self.qty_add_var.set(0)
+		self.id_add_var.set('')
 		
 		messagebox.showinfo("Stock Added", "Stock information has been saved!")
 		
@@ -152,10 +169,15 @@ class WS_Inventory(object):
 		self.inventory_list[index][1] = self.pn_var.get()
 		self.inventory_list[index][2] = self.nsn_var.get()
 		self.inventory_list[index][3] = self.qty_var.get()
+		try:
+			self.inventory_list[index][4] = self.id_var.get()
+		except IndexError:
+			self.inventory_list[index].append(self.id_var.get())
 		
 		self.pn_var.set('')
 		self.nsn_var.set('')
 		self.qty_var.set(0)
+		self.id_var.set('')
 		
 		win.destroy()
 		messagebox.showinfo("Stock Edited", "Stock information has been saved!")
@@ -167,12 +189,16 @@ class WS_Inventory(object):
 			
 	def edit_window(self, item):
 		e_window = tk.Toplevel()
-		e_window.geometry('250x130')
+		e_window.geometry('250x170')
 		e_window.title("Edit Stock")
 		
 		self.pn_var.set(item[1])
 		self.nsn_var.set(item[2])
 		self.qty_var.set(item[3])
+		try:
+			self.id_var.set(item[4])
+		except IndexError:
+			self.id_var.set('')
 		
 		pn_label = tk.Label(e_window, text="PN:")
 		pn_label.grid(row=0, column=0, padx=5, pady=5)
@@ -192,39 +218,51 @@ class WS_Inventory(object):
 		qty_entry = tk.Entry(e_window, width=5, textvariable=self.qty_var)
 		qty_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
 		
+		id_label = tk.Label(e_window, text="ID:")
+		id_label.grid(row=3, column=0, padx=5, pady=5)
+		
+		id_entry = tk.Entry(e_window, width=5, textvariable=self.id_var)
+		id_entry.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+		
 		submit_button = tk.Button(e_window, text="Submit", command=lambda: self.edit_stock(item[0]-1, e_window))
-		submit_button.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+		submit_button.grid(row=4, column=1, padx=5, pady=5, sticky="w")
 	
 	def add_stock_window(self):
 		win = tk.Toplevel()
-		win.geometry('250x130')
+		win.geometry('250x170')
 		win.title("Add Stock")
 		
 		pn_label = tk.Label(win, text="PN:")
 		pn_label.grid(row=0, column=0, padx=5, pady=5)
 		
-		pn_entry = tk.Entry(win, width=25, textvariable=self.pn_var)
+		pn_entry = tk.Entry(win, width=25, textvariable=self.pn_add_var)
 		pn_entry.grid(row=0, column=1, padx=5, pady=5, sticky="w")
 		
 		nsn_label = tk.Label(win, text="NSN:")
 		nsn_label.grid(row=1, column=0, padx=5, pady=5)
 		
-		nsn_entry = tk.Entry(win, width=15, textvariable=self.nsn_var)
+		nsn_entry = tk.Entry(win, width=15, textvariable=self.nsn_add_var)
 		nsn_entry.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 		
 		qty_label = tk.Label(win, text="Quantity:")
 		qty_label.grid(row=2, column=0, padx=5, pady=5)
 		
-		qty_entry = tk.Entry(win, width=5, textvariable=self.qty_var)
+		qty_entry = tk.Entry(win, width=5, textvariable=self.qty_add_var)
 		qty_entry.grid(row=2, column=1, padx=5, pady=5, sticky="w")
 		
+		id_label = tk.Label(win, text="ID:")
+		id_label.grid(row=3, column=0, padx=5, pady=5)
+		
+		id_entry = tk.Entry(win, width=5, textvariable=self.id_add_var)
+		id_entry.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+		
 		submit_button = tk.Button(win, text="Submit", command=self.add_stock)
-		submit_button.grid(row=3, column=1, padx=5, pady=5, sticky="w")
+		submit_button.grid(row=4, column=1, padx=5, pady=5, sticky="w")
 		
 	def inventory_window(self):
 		t = tk.Toplevel()
-		t.geometry('250x200')
-		t.title("Inventory Management")
+		t.geometry('275x75')
+		t.title("Stock Management")
 		
 		def _delete_window():
 			try:
@@ -238,12 +276,12 @@ class WS_Inventory(object):
 		self.pn_var.trace("w", self.update_view)
 		self.nsn_var.trace("w", self.update_view)
 		
-		search_button = tk.Button(t, text="Search Inventory", command=self.search_window)
-		search_button.grid(row=0, column=0, padx=10, pady=10)
+		search_button = tk.Button(t, text="Search Stock", command=self.search_window)
+		search_button.grid(row=0, column=0, padx=10, pady=5)
 		
 		add_button = tk.Button(t, text="Add Stock", command=self.add_stock_window)
-		add_button.grid(row=1, column=0, padx=10, pady=10)
+		add_button.grid(row=0, column=2, padx=10, pady=5)
 		
 		save_button = tk.Button(t, text="Save Changes", command=self.save_changes)
-		save_button.grid(row=2, column=0, padx=10, pady=10)
+		save_button.grid(row=1, column=1, padx=2, pady=5)
 		
---
+++ WS_Shipping.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from pathlib import Path
+from tkinter import messagebox
+
+# run through main_wb and grab PO number and contract number for each line
+# create another "search" window by PO number and contract number
+# when selecting contract, have two parts
+## labels for initial searching in VSM (first 6 / last 4 of contract number), label info (PN, Preservation Method), and company name for label saving
+## input for receiving report (toggle for total/partial, quantity, shipping/invoice number, total for verification, RFID number) with toggle for RFID
+
+class WS_Shipping(object):
+	
+	def __init__(self):
+	
+		self.contract_list = None
+		self.wip_list = None
+		
+		self.dict = None
+		self.main_wb = None
+		self.wip_wb = None
+		
+	def save_changes(self):
+		pass
+		
+	def create_lists(self):
+		pass
+		
+	def shipping_window(self):
+		
+		t = tk.TopLevel()
+		t.geometry('250x150')
+		t.title('Shipping Management')
+		
+		def _delete_window():
+			try:
+				self.save_changes()
+				t.destroy()
+			except:
+				pass
+				
+		t.protocol("WM_DELETE_WINDOW", _delete_window)
+		
+		t_path = Path('config_dict.json')
+		if t_path.is_file():
+			self.dict = json.load(open('config_dict.json'))
+			self.main_wb = openpyxl.load_workbook(self.dict['main'])
+			self.wip_wb = openpyxl.load_workbook(self.dict['wip'])
+			self.create_lists()
