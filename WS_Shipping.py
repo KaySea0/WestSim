2ecbee04bf5e9dab30c44b1a27d937f163e108f9
Kyle Cook <k.cook2499@gmail.com>
2019-05-10 12:57:14 -0400

    Shipping groundwork

--- WS_Shipping.py
+++ WS_Shipping.py
@@ -19,13 +19,35 @@ class WS_Shipping(object):
 		self.main_wb = None
 		self.wip_wb = None
 		
+		self.contract_var = tk.StringVar()
+		self.po_var = tk.StringVar()
+		
+		self.search_frame = None
+		self.canvas = None
+		self.scroll = None
+		
+	def update_view(self, *args):
+		pass
+		
 	def save_changes(self):
 		pass
 	
-	# start at 251
 	def create_lists(self):
 		main_ws = self.main_wb['DLAORDERS']
+		list_start = 278 # row number that search should start at for main_ws (dependent on what orders have come in)
+		list_end = main_ws.max_row+1
 		
+		# Contract Number - PO Number - Vendor Name
+		for i in range(list_start, list_end):
+			if not main_ws['I'+str(i)] is None:
+				self.contract_list.append([main_ws['B'+str(i)], main_ws['I'+str(i)], main_ws['F'+str(i)]])
+				
+		wip_ws = self.wip_wb.active
+		list_end = wip_ws.max_row+1
+		
+		# Contract Number - Vendor Name - P/N - Preservation Method
+		for i in range(1, list_end):
+			self.wip_list.append([wip_ws['B'+str(i)], wip_ws['G'+str(i)], wip_ws['H'+str(i)], wip_ws['I'+str(i)]])
 		
 	def shipping_window(self):
 		
@@ -48,3 +70,27 @@ class WS_Shipping(object):
 			self.main_wb = openpyxl.load_workbook(self.dict['main'])
 			self.wip_wb = openpyxl.load_workbook(self.dict['wip'])
 			self.create_lists()
+			
+		self.contract_var.trace("w", self.update_view)
+		self.po_var.trace("w", self.update_view)
+			
+		self.canvas = tk.Canvas(t, borderwidth=0)
+		self.scroll = tk.Scrollbar(t, orient="vertical", command=self.canvas.yview)
+		
+		input_frame = tk.Frame(t)
+		
+		contract_label = tk.Label(input_frame, text="Contract Number:")
+		contract_label.grid(row=0, column=0, padx=5, pady=5)
+		
+		contract_entry = tk.Entry(input_frame, width=30, textvariable=self.contract_var)
+		contract_entry.grid(row=0, column=1, padx=5, pady=5)
+		
+		po_label = tk.Label(input_frame, text="PO Number:")
+		po_label.grid(row=0, column=2, padx=5, pady=5)
+		
+		po_entry = tk.Entry(input_frame, width=10, textvariable=self.po_var)
+		po_entry.grid(row=0, column=3, padx=5, pady=5)
+		
+		input_frame.pack(anchor="n", fill=tk.X)
+		self.update_view()
+			
